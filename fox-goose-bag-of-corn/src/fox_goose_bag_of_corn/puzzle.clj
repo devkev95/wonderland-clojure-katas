cb77b526c1735efd494ac163e634e297439abc5f
Kevin Mejia <devkev95@gmail.com>
2018-04-17 16:00:23 -0600

    Finally getting near to solve this F***ING problem- PART 2 XD

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -9,13 +9,15 @@
         plan
         (let [[selected-bank other-bank bank-pos]
               (if (> dir 0) [lbank rbank 0] [rbank lbank 2])
-              r (vec (first (filter #(and
+              options (vec (map vec (filter #(and
                             (not= (set %) #{:fox :goose})
                             (not= (set %) #{:goose :corn}))
                           (map #(remove #{:you %1} %2)
                                [:fox :goose :corn]
                                (repeat selected-bank)))))
               o (vec (remove (set r) selected-bank))]
+          (let [res (loop [options options opt (first options)]
+                      )])
           (recur (conj plan
                        (apply assoc lstep [bank-pos r
                                            (+ bank-pos dir)
