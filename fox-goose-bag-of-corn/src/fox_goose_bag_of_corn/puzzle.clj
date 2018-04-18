15cab0907d50066e68059876b2fb8836e6a34792
Kevin Mejia <devkev95@gmail.com>
2018-04-18 16:00:37 -0600

    Gettin real F***ING close to solve fox-goose-corn riddle

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -9,20 +9,30 @@
         plan
         (let [[selected-bank other-bank bank-pos]
               (if (> dir 0) [lbank rbank 0] [rbank lbank 2])
-              options (vec (map vec (filter #(and
-                            (not= (set %) #{:fox :goose})
-                            (not= (set %) #{:goose :corn}))
-                          (map #(remove #{:you %1} %2)
-                               [:fox :goose :corn]
-                               (repeat selected-bank)))))
-              o (vec (remove (set r) selected-bank))]
-          (let [res (loop [options options opt (first options)]
-                      )])
-          (recur (conj plan
-                       (apply assoc lstep [bank-pos r
-                                           (+ bank-pos dir)
-                                           (apply conj boat o)])
-                       (apply assoc lstep [bank-pos r
-                                           (+ bank-pos (* 2 dir))
-                                           (apply conj other-bank o)]))
-                 (- dir)))))))
+              options (vec (map vec (filter
+                                     #(and
+                                       (not= (set %) #{:fox :goose})
+                                       (not= (set %) #{:goose :corn}))
+                                     (map #(remove #{:you %1} %2)
+                                          [:fox :goose :corn]
+                                          (repeat selected-bank)))))]
+          (let [res (loop [options options]
+                          (let [opt (first options)
+                                remain (vec (remove (set opt) selected-bank))
+                                poss-res [(apply assoc lstep [bank-pos opt
+                                                                   (+ bank-pos
+                                                                      dir)
+                                                                   (apply conj
+                                                                          boat
+                                                                          remain)])
+                                               (apply assoc lstep [bank-pos opt
+                                                                   (+ bank-pos
+                                                                      (* 2 dir))
+                                                                   (apply
+                                                                    conj
+                                                                    other-bank
+                                                                    remain)])]]
+                            (if (not-any? (set poss-res) (set plan))
+                              poss-res
+                              (recur (rest options)))))]
+          (recur (apply conj plan res) (- dir))))))))
