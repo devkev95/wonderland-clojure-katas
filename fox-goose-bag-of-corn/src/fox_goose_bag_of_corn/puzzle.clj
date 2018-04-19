5c7f7cab4511278b9d57b69f08b6ff71a2283c23
Kevin Mejia <devkev95@gmail.com>
2018-04-19 09:55:06 -0600

    Finally it's workingcd code/livinClojure/wonderland-clojure-katas/cd code/livinClojure/wonderland-clojure-katas/!

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -9,29 +9,30 @@
         plan
         (let [[selected-bank other-bank bank-pos]
               (if (> dir 0) [lbank rbank 0] [rbank lbank 2])
-              options (vec (map vec (filter
+              options (vec (sort-by count > (map vec (filter
                                      #(and
                                        (not= (set %) #{:fox :goose})
                                        (not= (set %) #{:goose :corn}))
                                      (map #(remove #{:you %1} %2)
                                           [:fox :goose :corn]
-                                          (repeat selected-bank)))))]
+                                          (repeat selected-bank))))))]
           (let [res (loop [options options]
-                          (let [opt (first options)
-                                remain (vec (remove (set opt) selected-bank))
-                                poss-res [(apply assoc lstep [bank-pos opt
+                          (let [remain (first options)
+                                opt (vec (remove (set opt) selected-bank))
+                                poss-res [(apply assoc lstep [bank-pos remain
                                                                    (+ bank-pos
                                                                       dir)
                                                                    (apply conj
                                                                           boat
-                                                                          remain)])
-                                               (apply assoc lstep [bank-pos opt
+                                                                          opt)])
+                                          (apply assoc lstep [bank-pos
+                                                              remain
                                                                    (+ bank-pos
                                                                       (* 2 dir))
                                                                    (apply
                                                                     conj
                                                                     other-bank
-                                                                    remain)])]]
+                                                                    opt)])]]
                             (if (not-any? (set poss-res) (set plan))
                               poss-res
                               (recur (rest options)))))]
