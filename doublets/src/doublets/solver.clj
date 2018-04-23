6d3938c0200217e1cb4cc5db22a347bd075cf3c5
Kevin Mejia <devkev95@gmail.com>
2018-04-23 15:57:02 -0600

    Maybe I can solve this before the meeting with Josh

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -10,24 +10,35 @@
                (read-string)))
 
 (defn word-buckets [word-list]
-  (loop [words word-list word (first word-list) res {}]
-    (if (nil? word)
-      res
-      (let [pairs
-            (map (fn [w]
-                   (let [n (count w)]
-                     (map #([(keyword
-                              (apply format "%s_%s"
-                                     (s/split w (-> w
-                                                    (get %)
-                                                    (str)
-                                                    (re-pattern))))) w])
-                            (take n (iterate inc 0)))))
-                 words)]
-        ))))
+  (let [pairs
+        (mapcat (fn [w]
+               (let [n (count w)]
+                 (map #(identity [(keyword
+                                   (apply format "%s_%s"
+                                          [(subs w 0 %)
+                                           (subs w (+ % 1))])) w])
+                      (take n (iterate inc 0)))))
+                      word-list)]
+    (reduce (fn [m pair]
+              (let [k (first pair) v (second pair)]
+                (assoc m k (cons v (get m k))))) {} pairs)))
 
-(defn words-graph [word-list])
+(defn words-graph [word-list]
+  (let [words-bucket (word-buckets word-list)]
+    (filter not-empty (mapcat (fn [[_ v]]
+                                (map #(identity [%1 %2]) v (rest v)))
+                              words-bucket))))
 
 (defn doublets [word1 word2]
-  (let [words-list (filter #())])
-  )
+  (if (= (count word1) (count word2))
+    (let [n (count (word1))
+          words-list (filter #(= n (count %)) words)
+          graph (words-graph words-graph)]
+      (tree-seq #(not= (last %) word2)
+                (fn [doublet]
+                  (->> graph
+                       (filter (fn [v]
+                                 (and (some #{(last doublet)} v)
+                                      (not-any? #(some #{%} v)
+                                                (drop-last doublet))))))))
+    []))
